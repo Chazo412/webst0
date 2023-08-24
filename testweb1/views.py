952543ac881cfa4786b9468e33a674b9f994370c
Chazo412 <Bannawi@LAPTOP-2MTUI9FC>
2023-08-24 19:41:22 +0800

    extract video id

--- testweb1/views.py
+++ testweb1/views.py
@@ -8,6 +8,8 @@ from django.conf import settings
 from django.http import JsonResponse
 from .forms import VideoIdForm  # Import your VideoIdForm
 import requests
+import time
+import re
 
 
 def sample(request):
@@ -41,31 +43,59 @@ def sample(request):
 
 
 
+def mp3_conversion(video_id, api_key, api_host):
+    api_key = settings.API_KEY
+    api_host = settings.API_HOST
+    url = f"https://{api_host}/dl?id={video_id}"
+
+    headers = {
+        "x-rapidapi-key": api_key,
+        "x-rapidapi-host": api_host
+    }
+
+    while True:
+        response = requests.get(url, headers=headers)
+        fetch_response = response.json()
+
+        if fetch_response["status"] == "processing":
+            time.sleep(1)
+        else:
+            return fetch_response
+
+def extract_video_id(url):
+    # Define the regular expression pattern to match YouTube video IDs
+    pattern = r"(?:v=|\/videos\/|embed\/|youtu.be\/|\/v\/|\/e\/|watch\?v=|&v=)([\w-]+)"
+
+    # Use re.search to find the video ID in the URL
+    match = re.search(pattern, url)
+
+    if match:
+        return match.group(1)
+    else:
+        return None
+
 def convert_mp3(request):
     form = VideoIdForm(request.POST)
     if form.is_valid():
-        video_id = form.cleaned_data['video_id']
+        youtube_url = form.cleaned_data['video_id']  # Get the YouTube URL from the form
 
-        if not video_id:
-            return render(request, 'uiDesign.html', {'success': False, 'message': 'Please enter a video ID'})
+        # Extract the video ID from the URL using the updated function
+        video_id = extract_video_id(youtube_url)
+        
+        if video_id:
+            api_key = settings.API_KEY
+            api_host = settings.API_HOST
 
-        api_key = settings.API_KEY  # Access the API key from settings
-        api_host = settings.API_HOST  # Access the API host from settings
+            response = mp3_conversion(video_id, api_key, api_host)
 
-        response = requests.get(f'https://{api_host}/dl?id={video_id}', 
-                                headers={"x-rapidapi-key": api_key, "x-rapidapi-host": api_host})
-
-        fetch_response = response.json()
-
-        if fetch_response.get('status') == 'ok':
-            return render(request, 'uiDesign.html', {'success': True, 
-                                                    'song_title': fetch_response.get('title'), 
-                                                    'song_link': fetch_response.get('link')})
+            if response.get('status') == 'ok':
+                return render(request, 'uiDesign.html', {'success': True,
+                                                         'song_title': response.get('title'),
+                                                         'song_link': response.get('link')})
+            else:
+                return render(request, 'uiDesign.html', {'success': False, 'message': response.get('msg')})
         else:
-            return render(request, 'uiDesign.html', {'success': False, 'message': fetch_response.get('msg')})
+            return render(request, 'uiDesign.html', {'success': False, 'message': 'Invalid YouTube URL'})
 
     return render(request, 'uiDesign.html', {'form': form})
-
-
-
              
